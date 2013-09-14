662b83cb4aa933a3aad5f878cb03d00a82f8cbe9
tjsnell <tjs@juicelabs.com>
2013-09-13 22:51:14 -0400

    clean up code. Sort Changed list

--- src/main/java/cc/notsoclever/tools/Versions.java
+++ src/main/java/cc/notsoclever/tools/Versions.java
@@ -68,7 +68,9 @@ public class Versions {
       Collections.sort(changed, new Comparator<Object>() {
          @Override
          public int compare(Object o1, Object o2) {
-            return ((String) o1).compareToIgnoreCase((String) o2);
+            ArrayList<String> a1 = (ArrayList<String>) o1;
+            ArrayList<String> a2 = (ArrayList<String>) o2;
+            return (a1.get(0)).compareToIgnoreCase(a2.get(0));
          }
       });
    }
