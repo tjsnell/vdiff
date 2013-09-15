51012bf84695b1b9f10d9066c7fdcf651dc42c05
tjsnell <tjs@juicelabs.com>
2013-09-15 01:35:35 -0400

    wrap jquery calls

--- src/main/webapp/DiffCtrl.js
+++ src/main/webapp/DiffCtrl.js
@@ -1,32 +1,44 @@
 function DiffCtrl($scope, $http) {
 
     $scope.diff = function () {
-        $("body").css("cursor", "wait");
+        $scope.$evalAsync(
+            $("body").css("cursor", "wait")
+        );
         $http.get("/vdiff/" + $scope.v1 + "/" + $scope.v2)
             .success(function (data, status, headers, config) {
                 $scope.added = sortObject(data.added);
                 $scope.dropped = sortObject(data.dropped);
                 $scope.changed = data.changed;
                 $scope.unchanged = sortObject(data.unChanged);
-                $("body").css("cursor", "default");
+                $scope.$evalAsync(
+                    $("body").css("cursor", "default")
+                )
             })
             .error(function (data, status, headers, config) {
                 console.log("Status: " + status);
-                $("body").css("cursor", "default");
+                $scope.$evalAsync(
+                    $("body").css("cursor", "default")
+                );
             });
 
     };
 
     $scope.init = function () {
-        $("body").css("cursor", "wait");
+        $scope.$evalAsync(
+            $("body").css("cursor", "wait")
+        );
         $http.get("/vdiff/tags")
             .success(function (data, status, headers, config) {
                 $scope.optionsList = angular.fromJson(data);
-                $("body").css("cursor", "default");
+                $scope.$evalAsync(
+                    $("body").css("cursor", "default")
+                )
             })
             .error(function (data, status, headers, config) {
                 console.log("Status-init: " + status);
-                $("body").css("cursor", "default");
+                $scope.$evalAsync(
+                    $("body").css("cursor", "default")
+                )
             });
     };
 
